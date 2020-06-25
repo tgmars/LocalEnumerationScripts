54e997fc37d9df97b218f79c83e2f9073816d5d7
tgmars <tmarsonearth@gmail.com>
2020-06-25 11:21:04 +1000

    Add WMIC short description of the STARTUP class

--- batch/startup.bat
+++ batch/startup.bat
@@ -2,5 +2,6 @@
 SETLOCAL ENABLEEXTENSIONS
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+REM STARTUP - Management of commands that run automatically when users log onto the computer system.
 echo "startup.bat
 wmic STARTUP GET Caption, Command, User, Location /FORMAT:CSV > .\batch\output\startup.csv
