9c26ab5a9ca02541098fe51a2d665134d542952e
tgmars <tmarsonearth@gmail.com>
2020-07-03 21:57:05 +1000

    Bash - initial commit for get_accounts

---
+++ bash/get_accounts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+#
+# Refs:
+
+# Alternative just cat /etc/passwd
+printf "username,groups" > ./output/accounts.csv
+for username in $(getent passwd | awk -F":" '{ print $1}') 
+do
+    sudo groups $username | sed -e "s/:/,/g" >> ./output/accounts.csv
+done
+
