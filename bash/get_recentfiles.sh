89b230385317bf388073807a02193d47972e73ec
tgmars <tmarsonearth@gmail.com>
2020-07-03 22:31:53 +1000

    Bash - initial commit for get_recentfiles

---
+++ bash/get_recentfiles.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+# Gets a list of file smodified in the last 1440 mins (24 hours), excluding those in /proc and /sys
+sudo find / -not \( -path /proc -prune \) -not \( -path /sys -prune \) -mmin -1440 2>/dev/null | tee ./output/recentfiles.txt
