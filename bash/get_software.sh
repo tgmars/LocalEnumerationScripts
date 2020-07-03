f49d9b4f1d4f58d2c70a9d86ed8d70ca1bdaabb7
tgmars <tmarsonearth@gmail.com>
2020-07-03 22:32:13 +1000

    Bash - initial commit for get_software

---
+++ bash/get_software.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+#
+# Uses dpkg to list installed packages on the system
+
+# dpkgVar=$(dpkg --list)
+
+# printf "%s\n" "$dpkgVar" > ./output/software.txt
+
+declare -a package
+declare -a version
+
+while read -a line ; do
+    if [ ${line[0]} = "ii" ]
+    then
+        package+=( ${line[1]} )
+        version+=( ${line[2]} )
+    fi
+done < <( dpkg --list )
+
+# array_to_csv $1 ... echos an arrays values separated by columns on a single line
+
+printf "package,version\n" | tee ./output/software.csv
+
+for ((i=0; i<${#package[@]}; i++)); do
+    printf "%s,%s\n" "${package[i]}" "${version[i]}" | tee -a ./output/software.csv
+done
+
+
