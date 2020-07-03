9e437868162dbd7c228b57fcb9e2dab6f021d7e5
tgmars <tmarsonearth@gmail.com>
2020-07-03 21:56:47 +1000

    Bash - initial commit for get_time

---
+++ bash/get_time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+# 
+# Get the date in RFC-3339 format and output to text file. This output includes the timezone of the local system ()
+# To extend this, cat /etc/timezone to get a textual representation of the timezone.
+
+dateVar=$(date --rfc-3339='seconds') 
+textTZ=$(cat /etc/timezone)
+printf "datetime,texttimezone\n%s,%s\n" "$dateVar" "$textTZ" | tee ./output/datetimezone.txt
+
