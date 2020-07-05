c9da466bc8017118251c697114a9aa04338660c9
tgmars <tmarsonearth@gmail.com>
2020-07-05 23:36:57 +1000

    Bash - initial commit get_filecontent

--- bash/get_filecontent.sh
+++ bash/get_filecontent.sh
@@ -1,2 +1,20 @@
-# dd if=/abs/olute/path of=yourfile.raw
+#!/bin/bash -e
+#Given a file specified in positional parameter 1, copy a byte-by-byte replica of 
+#the file to the current directory. Prompts the user if the hash has changed in the process
+ 
+if [ "$1" == "" ]; then
+    printf "Positional parameter 1 is empty.\n"
+    exit 1
+fi
+
+fileToCopy=$1
+outputFilename=$(basename "$fileToCopy")
+ 
+initHash=$(sha1sum $fileToCopy | awk '{ print $1 }')
+dd if=$fileToCopy of=./$outputFilename
+postHash=$(sha1sum ./$outputFilename | awk '{ print $1 }')
+
+if [ "$initHash" != "$postHash" ]; then 
+    printf "Hashes don't match.\n"
+fi
 
