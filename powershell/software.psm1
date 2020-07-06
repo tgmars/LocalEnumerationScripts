be55036072ed195aa998b674280c7b1a98121cda
Tom Desktop <tomgmarsden@gmail.com>
2020-07-06 16:18:32 +1000

    PS - Generic loop to query from specified keys

--- powershell/software.psm1
+++ powershell/software.psm1
@@ -1,17 +1,39 @@
 function Get-InstalledSoftware {
+    <#
+    .SYNOPSIS
+        Returns installed software using Uninstall keys in SOFTWARE\Microsoft and
+        SOFTWARE\WoW6432Node
+    #>    
     [CmdletBinding()]
     param()
     begin {
         Write-Host($PSCmdlet.MyInvocation.MyCommand.Name)
     }
     process {
-        $localVariable = (Get-CimInstance -ClassName Win32_Product) | Select-Object Name, PackageName, Version, Vendor, InstallDate, InstallSource
-        Write-Host($localVariable)
-
-        # Stub
-        # $lvJSON = ConvertTo-Json($localVariable)
     }
-    end {    
+    end {
+
+        $QueriesToEnumerate=@(
+            "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*",
+            "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        )   
+        $InstalledSoftware=@()
+ 
+        foreach ($query in $QueriesToEnumerate){
+            $QueryResults=Get-ItemProperty $query
+            foreach ($key in $QueryResults) {
+                $InstalledSoftware+=[PSCustomObject]@{
+                    SourceKey=$key.PSParentPath;
+                    DisplayName=$key.DisplayName;
+                    Version=$key.DisplayVersion;
+                    Publisher=$key.Publisher;
+                    InstallDate=$key.InstallDate;
+                }
+            }
+        }
+        return $InstalledSoftware
     }
 }
+
+Export-ModuleMember -Function Get-InstalledSoftware
 
