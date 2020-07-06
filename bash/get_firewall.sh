38cfb27cf771a4e0a53365e7809fcf1683e7d639
tgmars <tmarsonearth@gmail.com>
2020-07-06 11:15:55 +1000

    Bash - add @prop headers for each entry

--- bash/get_firewall.sh
+++ bash/get_firewall.sh
@@ -7,19 +7,19 @@
 
 # If the above command isn't working, this is a suitable alternative.
 filterTable=$(sudo iptables --table filter --list -n -v) 
-printf "FilterTable\n%s\n" "$filterTable"  > ./output/firewall.txt
+printf "@prop\ntype=FilterTable\n%s\n" "$filterTable"  > ./output/firewall.txt
 
 natTable=$(sudo iptables --table nat --list -n -v) 
-printf "NatTable\n%s\n" "$natTable"  >> ./output/firewall.txt
+printf "@prop\ntype=NatTable\n%s\n" "$natTable"  >> ./output/firewall.txt
 
 mangleTable=$(sudo iptables --table mangle --list -n -v) 
-printf "MangleTable\n%s\n" "$mangleTable"  >> ./output/firewall.txt
+printf "@prop\ntype=MangleTable\n%s\n" "$mangleTable"  >> ./output/firewall.txt
 
 rawTable=$(sudo iptables --table raw --list -n -v) 
-printf "RawTable\n%s\n" "$rawTable"  >> ./output/firewall.txt
+printf "@prop\ntype=RawTable\n%s\n" "$rawTable"  >> ./output/firewall.txt
 
 securityTable=$(sudo iptables --table security --list -n -v) 
-printf "SecurityTable\n%s\n" "$securityTable"  >> ./output/firewall.txt
+printf "@prop\ntype=SecurityTable\n%s\n" "$securityTable"  >> ./output/firewall.txt
 
 
 
