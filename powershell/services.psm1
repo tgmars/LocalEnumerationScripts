4ce65148d167e0486b516063f119ce3eeadcffcb
Tom Desktop <tomgmarsden@gmail.com>
2020-07-07 09:13:05 +1000

    PS - Query Win32_Services API with CIM

--- powershell/services.psm1
+++ powershell/services.psm1
@@ -1,18 +1,29 @@
 function Get-Services {
+    <#
+    .SYNOPSIS
+        Returns services and their state on the current host
+    #>    
     [CmdletBinding()]
     param()
     begin {
         Write-Host($PSCmdlet.MyInvocation.MyCommand.Name)
     }
     process {
-        $localVariable = (Get-CimInstance -ClassName Win32_Service) | Select-Object Name, DisplayName, Description, 
-            InstallDate, PathName, ProcessId, State, Status, SystemName, StartMode
-        Write-Host($localVariable)
-
-        # Stub
-        # $lvJSON = ConvertTo-Json($localVariable)
     }
     end {    
+        $Win32Services = (Get-CimInstance -ClassName Win32_Service) 
+        return [PSCustomObject]@{
+            Name=$Win32Services | Select-Object Name; 
+            DisplayName=$Win32Services | Select-Object DisplayName; 
+            Description=$Win32Services | Select-Object Description; 
+            InstallDate=$Win32Services | Select-Object InstallDate;
+            Imagename=$Win32Services | Select-Object PathName;
+            PID=$Win32Services | Select-Object ProcessId;
+            State=$Win32Services | Select-Object State;
+            Status=$Win32Services | Select-Object Status;
+            Systemname=$Win32Services | Select-Object SystemName;
+            Startmode=$Win32Services | Select-Object StartMode
+        }
     }
 }
 
