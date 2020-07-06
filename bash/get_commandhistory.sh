35d091e30414576b94a94e11ce778c2feae78796
tgmars <tmarsonearth@gmail.com>
2020-07-06 11:15:37 +1000

    Bash - parse command history from .bash_history files of all users

--- bash/get_commandhistory.sh
+++ bash/get_commandhistory.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -e
+# Runs history and grabs content of all users .bash_history files
 
-history > ./output/command_history.txt
+cmdlinehistory="./output/command_history.txt"
+
+printf "@prop\ntype=history\n" > $cmdlinehistory
+history >> "$cmdlinehistory"
+
+for uname in $(getent passwd | awk -F":" '{ print $1}') 
+do
+    if [ -d /home/$uname ]; then
+        printf "@prop\nuser=$uname\ntype=.bash_history\n" >> $cmdlinehistory
+        cat "/home/$uname/.bash_history" >> $cmdlinehistory
+    fi
+
+done
