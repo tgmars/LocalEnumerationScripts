efa9e19f770633e9af41121f1a84cef87c2e3e05
tgmars <tmarsonearth@gmail.com>
2020-07-06 11:17:54 +1000

    Bash - retrieve forensically relevant information about recent files

--- bash/get_recentfiles.sh
+++ bash/get_recentfiles.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -e
 # Gets a list of file smodified in the last 1440 mins (24 hours), excluding those in /proc and /sys
-sudo find / -not \( -path /proc -prune \) -not \( -path /sys -prune \) -mmin -1440 2>/dev/null > ./output/recentfiles.txt
+
+recentfiles="./output/recentfiles.txt"
+
+if [ -f $recentfiles ]; then
+    rm $recentfiles
+    touch $recentfiles
+fi
+
+for recentfile in $(sudo find / -not \( -path /proc -prune \) -not \( -path /sys -prune \) -mmin -1440 -type f 2>/dev/null)
+do
+    sudo ls -lhd $recentfile >> $recentfiles
+done 
