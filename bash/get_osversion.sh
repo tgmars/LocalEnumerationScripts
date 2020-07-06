6af69ccaaad5b867832029bbcca5769b0a33ce59
tgmars <tmarsonearth@gmail.com>
2020-07-06 11:17:28 +1000

    Bash - safely handle os_version output file create and delete

--- bash/get_osversion.sh
+++ bash/get_osversion.sh
@@ -3,6 +3,18 @@
 # Get OS Version ... writes lsb_release info on the OS to osversion.csv
 # and the uname to query hardware and kernel details to kernel.csv
 
+# Prepare output files
+
+if [ -f "./output/osversion.csv" ]; then
+    rm ./output/osversion.csv
+    touch ./output/osversion.csv
+fi
+
+if [ -f "./output/kernel.csv" ]; then
+    rm ./output/kernel.csv
+    touch ./output/kernel.csv
+fi
+
 # Split on colons in lsb_release output
 IFS=':'
 # Declare an array called headers, used to store the headers
@@ -16,13 +28,13 @@ while IFS=':' read -a line ; do
     # Strip whitespace away from values
     valueNoSpace="$(echo -e "${line[1]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
     values+=( $valueNoSpace )
-done < <( lsb_release -a )
+done < <( lsb_release -a 2>/dev/null )
 
 # array_to_csv $1 ... echos an arrays values separated by columns on a single line
 array_to_csv () {
     localArray=("$@")
     for index in "${localArray[@]}"; do printf "%s," $index >> ./output/osversion.csv; done
-    printf "\n" >> ./output/osversion
+    printf "\n" >> ./output/osversion.csv
 }
 
 # Write each of the 'rows' out to csv.
@@ -37,7 +49,7 @@ kernelName=$(uname -s)
 kernelRelease=$(uname -r)
 kernelVersion=$(uname -v)
 machineArchitecture=$(uname -m)
-printf "hostname,kernelname,kernelrelease,kernelversion,machinearchitecture\n%s,%s,%s,%s,%s" \
+printf "hostname,kernelname,kernelrelease,kernelversion,machinearchitecture\n%s,%s,%s,%s,%s\n" \
     "$hostname" "$kernelName" "$kernelRelease" "$kernelVersion" "$machineArchitecture" >> ./output/kernel.csv
 
 
