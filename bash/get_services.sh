e2d726255cb1d604a37d19a96b6a6e821b4368c1
tgmars <tmarsonearth@gmail.com>
2020-07-06 10:17:28 +1000

    Bash - prep services output file

--- bash/get_services.sh
+++ bash/get_services.sh
@@ -3,5 +3,8 @@
 # Refs:
 # https://askubuntu.com/questions/407075/how-to-read-service-status-all-results
 
+rm ./output/services-services.txt
+touch ./output/services-services.txt
+
 sudo service --status-all >>./output/services-services.txt 2>&1
 sudo initctl list > ./output/upstart-services.txt
