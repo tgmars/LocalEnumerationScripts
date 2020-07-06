146f5f29d45da054abb3d0c96bbeae986285ca7c
tgmars <tmarsonearth@gmail.com>
2020-07-06 10:14:00 +1000

    Bash - convert dmesg timestamp to human-interpretable

--- bash/get_usb.sh
+++ bash/get_usb.sh
@@ -1,3 +1,27 @@
 #!/bin/bash -e
 # 
-dmesg | grep -i usb | tee ./output/usb_history.txt
+# Note! Old versions of dmesg don't support the -T flag.
+# Refs:
+# https://stackoverflow.com/questions/13890789/convert-dmesg-timestamp-to-custom-date-format
+
+# Absolutely stolen from Allen Belletti's answer in the above SO link.
+dmesg_with_human_timestamps () {
+    FORMAT="%a %b %d %H:%M:%S %Y"
+
+    now=$(date +%s)
+    cputime_line=$(grep -m1 "\.clock" /proc/sched_debug)
+
+    if [[ $cputime_line =~ [^0-9]*([0-9]*).* ]]; then
+        cputime=$((BASH_REMATCH[1] / 1000))
+    fi
+
+    dmesg | while IFS= read -r line; do
+        if [[ $line =~ ^\[\ *([0-9]+)\.[0-9]+\]\ (.*) ]]; then
+            stamp=$((now-cputime+BASH_REMATCH[1]))
+            echo "[$(date +"${FORMAT}" --date=@${stamp})] ${BASH_REMATCH[2]}"
+        else
+            echo "$line"
+        fi
+    done
+}
+dmesg_with_human_timestamps | grep -i usb | tee ./output/usb_history.txt
