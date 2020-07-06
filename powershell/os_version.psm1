00c0e40ccf9f49e6e2b804a6e18652b2ccb0cb64
Tom Desktop <tomgmarsden@gmail.com>
2020-07-06 15:19:33 +1000

    PS - Add synopsis and return custom ps obj for osversion

--- powershell/os_version.psm1
+++ powershell/os_version.psm1
@@ -1,13 +1,23 @@
 function Get-OSVersion {
+    <#
+    .SYNOPSIS
+        Returns OS build number and architecture information.
+    #>    
     [CmdletBinding()]
     param()
     begin {
         Write-Host($PSCmdlet.MyInvocation.MyCommand.Name)
     }
     process {
-        $localVariable = (Get-CimInstance Win32_OperatingSystem).Caption
+        $localVariable = 
         Write-Host($localVariable)
     }
     end {
+        return [PSCustomObject]$OSVersion=@{
+            FriendlyName=(Get-CimInstance Win32_OperatingSystem).Caption;
+            Version=(Get-CimInstance Win32_OperatingSystem).Version;
+            Architecture=(Get-CimInstance Win32_OperatingSystem).OSArchitecture
+            EncryptionLevel=(Get-CimInstance Win32_OperatingSystem).EncryptionLevel
+        }
     }
 }
