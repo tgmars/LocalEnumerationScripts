109b0b8430445ed8d7ec82cdeaf82e8c96f53db6
Tom Desktop <tomgmarsden@gmail.com>
2020-07-06 17:15:10 +1000

    PS - CIM and .NET view of processes in custom obj

--- powershell/processes.psm1
+++ powershell/processes.psm1
@@ -1,17 +1,41 @@
-function Get-Processes{
+function Get-Processes {
+    <#
+    .SYNOPSIS
+        Returns process information derived from .NET and Win32_Process 
+        classes. This is pretty slow because we're enumerating each PID
+        and making a call to .NET and CIM for each PID, taking data from
+        each and mashing them into a PSCustomObject.
+    #>        
     [CmdletBinding()]
     param()
     begin {
         Write-Host($PSCmdlet.MyInvocation.MyCommand.Name)
     }
     process {
-        $localVariable = (Get-CimInstance -ClassName Win32_Process)
-        Write-Host($localVariable)
-
-        # Stub
-        # $lvJSON = ConvertTo-Json($localVariable)
     }
-    end {    
+    end {
+        $PIDs=Get-Process | Select-Object Id
+        
+        $CurrentProcs=@()
+
+        foreach ($pid in $PIDs.Id){
+            $Win32Proc=Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $pid"
+            $DotNETProc=[System.Diagnostics.Process]::GetProcessById($pid)
+            $CurrentProcs+=[PSCustomObject]@{
+                PID=$Win32Proc.ProcessId;
+                PPID=$Win32Proc.ParentProcessId;
+                ImagePath=$Win32Proc.ExecutablePath;
+                CommandLine=$Win32Proc.CommandLine;
+                CreationDate=$Win32Proc.CreationDate;
+                ProcName=$DotNETProc.ProcessName;
+                Threads=$DotNETProc.Threads;
+                Modules=$DotNETProc.Modules;
+                Handles=$DotNETProc.Handles;
+            }
+        }
+        return $CurrentProcs
     }
 }
+
+Export-ModuleMember -Function Get-Processes
 
