a78ac553dc3591e2e233d931852179b880bff619
tgmars <tmarsonearth@gmail.com>
2020-07-06 11:37:07 +1000

    Bash - ensure output is written and markup with props

--- bash/get_tasks.sh
+++ bash/get_tasks.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -e
 
-rm ./output/cron.txt 2>/dev/null
-touch ./output/cron.txt
+cronout="./output/cron.txt"
+
+if [ -f $cronout ]; then 
+    rm $cronout
+    touch $cronout
+fi
 
 IFS=$'\n'
 for uname in $(getent passwd | awk -F":" '{ print $1}') 
 do
-    for cronentry in $(sudo crontab -u $uname -l 2>/dev/null) 
+    for cronentry in $(sudo crontab -u $uname -l 2>/dev/null | grep -v '^#') 
     do 
-        printf "%s\n%s\n" $uname $cronentry >> ./output/cron.txt
+        printf "@prop\nuser=%s\ntype=usercrontab\n%s\n" $uname $cronentry >> $cronout
     done
 done
 
-contentsCrontab=$(cat /etc/crontab)
-printf "%s\n%s" "crontab" $contentsCrontab >> ./output/cron.txt
+contentsCrontab=$(cat /etc/crontab | grep -v '^#')
+printf "@prop\ntype=/etc/crontab\n%s\n" $contentsCrontab >> $cronout
