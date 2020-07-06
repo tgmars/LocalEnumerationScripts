005bde5d651f426626be1f1183725680c2f6e57f
Tom Desktop <tomgmarsden@gmail.com>
2020-07-06 15:17:39 +1000

    PS - add synopsis and return custom obj for timezone

--- powershell/timezone.psm1
+++ powershell/timezone.psm1
@@ -1,15 +1,18 @@
 function Get-TZ {
+    <#
+    .SYNOPSIS
+        Returns the offset from UTC time of the current system.
+    #>
     [CmdletBinding()]
     param()
     begin {
         Write-Host($PSCmdlet.MyInvocation.MyCommand.Name)
     }
     process {
-        $localVariable = (Get-TimeZone) | Select-Object  BaseUtcOffset 
-        Write-Host($localVariable)
-        # Stub
-        # $lvJSON = ConvertTo-Json($localVariable)
     }
     end {
+        return [PSCustomObject]$TZ=@{
+            TimeZone=(Get-TimeZone) | Select-Object BaseUtcOffset 
+        }
     }
 }
