72d085a3ce88df66350f299ca7f4a17f667e9771
Tom Desktop <tomgmarsden@gmail.com>
2020-07-06 16:19:13 +1000

    PS - add function for displaying nested objects

--- powershell/utils.psm1
+++ powershell/utils.psm1
@@ -9,3 +9,19 @@ function Write-Object{
     )
     Write-Host( $objOutput | Format-List | Out-String)
 }
+
+# Write-NestedObject(PSObject) ... writes the specified object to STDOUT in
+# truncated, expanded JSON as a string.
+function Write-NestedObject{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [PSObject]
+        $objOutput
+    )
+    
+    Write-Host( $objOutput | Format-Custom | Out-String)
+}
+
+Export-ModuleMember -Function Write-NestedObject
+Export-ModuleMember -Function Write-Object
