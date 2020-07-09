6588cedaa9c997affd0d115ec5c4e5a0426653c1
tgmars <tmarsonearth@gmail.com>
2020-07-09 15:06:14 +1000

    PS - autoruns initial commit

--- powershell/autoruns.psm1
+++ powershell/autoruns.psm1
@@ -1,3 +1,17 @@
-# PSProfile
-# Autoruns w sysinternals?
-# Path ENVAR
+function Get-Autoruns {
+    <#
+    .SYNOPSIS
+        Gets commands executed on startup using Win32_StartupCommand & enumerating profiles
+    #>
+    [CmdletBinding()]
+    param()
+    begin {
+        Write-Host($PSCmdlet.MyInvocation.MyCommand.Name)
+    }
+    process {
+    }
+    end {
+        return (Get-CimInstance -ClassName Win32_StartupCommand)
+    }
+}
+Export-ModuleMember -Function Get-Autoruns
