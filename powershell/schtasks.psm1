fb67fe3bf9fe667450edfc55ca2d71d70a047ad3
tgmars <tmarsonearth@gmail.com>
2020-07-10 22:56:45 +1000

    PS - mitigate attempting to parse non-tasks in the task directories

--- powershell/schtasks.psm1
+++ powershell/schtasks.psm1
@@ -2,6 +2,7 @@ function Get-SchedTasks {
     <#
     .SYNOPSIS
         Retrieve scheduled tasks for all users from systemroot\tasks and systemroot\system32\tasks.
+        Returns an array of XML objects, these need to be looped through to access them.
     .REFERENCES
     #>        
     [CmdletBinding()]
@@ -16,20 +17,24 @@ function Get-SchedTasks {
         $System32TaskPath="$env:SystemRoot\System32\Tasks\"
         $SystemRootTaskPath="$env:SystemRoot\Tasks\"
 
-        Get-ChildItem -Path $System32TaskPath -Recurse -File -Force  | Select-Object FullName | ForEach-Object {
-            $CurrentPath=$_.FullName
-            # Write-Host($CurrentPath)
-            [XML]$tempvar=Get-Content $CurrentPath
-            $Tasks+=$tempvar
-            # Write-Object($tempvar.Task.RegistrationInfo)
+        Get-ChildItem -Path $System32TaskPath -Recurse -File -Force  | Select-Object FullName,Name,BaseName | ForEach-Object {
+            if($_.Name -eq $_.BaseName){
+                $CurrentPath=$_.FullName
+                # Write-Host($CurrentPath)
+                [XML]$tempvar=Get-Content $CurrentPath -ErrorAction SilentlyContinue
+                $Tasks+=$tempvar
+                # Write-Object($tempvar.Task.RegistrationInfo)
+            }
         } 
 
-        Get-ChildItem -Path $SystemRootTaskPath -Recurse -File -Force  | Select-Object FullName | ForEach-Object {
-            $CurrentPath=$_.FullName
-            # Write-Host($CurrentPath)
-            [XML]$tempvar=Get-Content $CurrentPath
-            $Tasks+=$tempvar
-            # Write-Object($tempvar.Task.RegistrationInfo)
+        Get-ChildItem -Path $SystemRootTaskPath -Recurse -File -Force  | Select-Object FullName,Name,BaseName | ForEach-Object {
+            if($_.Name -eq $_.BaseName){
+                $CurrentPath=$_.FullName
+                # Write-Host($CurrentPath)
+                [XML]$tempvar=Get-Content $CurrentPath -ErrorAction SilentlyContinue
+                $Tasks+=$tempvar
+                # Write-Object($tempvar.Task.RegistrationInfo)
+            }
         } 
         return $Tasks
         }
